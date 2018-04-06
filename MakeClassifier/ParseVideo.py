4b7d3b0511de3ac03d300249448876843990a763
00Bren00 <ringelb8@students.rowan.edu>
2018-04-06 17:11:32 -0400

    removed bad not-dog pictures - dog pictures should be good

--- MakeClassifier/ParseVideo.py
+++ MakeClassifier/ParseVideo.py
@@ -2,12 +2,12 @@ import cv2
 
 print(cv2.__version__)
 
-vidcap = cv2.VideoCapture(r'C:\Users\Resea\PycharmProjects\OpenCV\dog-in-chair.mp4')
+vidcap = cv2.VideoCapture(r'C:\Users\Resea\PycharmProjects\Dog-QuestionMark-\Videos\dog-in-chair.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
 while success:
-   cv2.imwrite(r"C:\Users\Resea\PycharmProjects\OpenCV\dog\dogInChairFrame%d.jpg" % count, image)     # save frame as JPEG file
+   cv2.imwrite(r"C:\Users\Resea\PycharmProjects\Dog-QuestionMark-\Photos\dog\dogInChairFrame%d.jpg" % count, image)     # save frame as JPEG file
    success,image = vidcap.read()
    print 'Read a new frame: ', success
    count += 1
