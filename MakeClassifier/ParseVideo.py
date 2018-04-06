f32a1925b259f6f9477b4333a74f3a7d340e96b5
00Bren00 <ringelb8@students.rowan.edu>
2018-04-06 18:19:31 -0400

    added cat photos

--- MakeClassifier/ParseVideo.py
+++ MakeClassifier/ParseVideo.py
@@ -2,12 +2,12 @@ import cv2
 
 print(cv2.__version__)
 
-vidcap = cv2.VideoCapture(r'C:\Users\Resea\PycharmProjects\Dog-QuestionMark-\Videos\not-dog-just-chair.mp4')
+vidcap = cv2.VideoCapture(r'C:\Users\Resea\PycharmProjects\Dog-QuestionMark-\Videos\cat_video.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
 while success:
-   cv2.imwrite(r"C:\Users\Resea\PycharmProjects\Dog-QuestionMark-\Photos\not-dog\dogNotInChairFrame%d.jpg" % count, image)     # save frame as JPEG file
+   cv2.imwrite(r"C:\Users\Resea\PycharmProjects\Dog-QuestionMark-\Photos\not-dog\cat\cat%d.jpg" % count, image)     # save frame as JPEG file
    success,image = vidcap.read()
    print 'Read a new frame: ', success
    count += 1
