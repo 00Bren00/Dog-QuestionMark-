eb8a9283a24c6e13fe658534e17cc0c09a632182
00Bren00 <ringelb8@students.rowan.edu>
2018-04-06 18:08:02 -0400

    not-dog pictures should be good - does not include cat pics

--- MakeClassifier/Driver.py
+++ MakeClassifier/Driver.py
@@ -2,9 +2,28 @@ import ParsePhotoBB
 from sklearn.neural_network import MLPClassifier
 from PIL import Image
 
-list = ParsePhotoBB.parse_photo_BW(r"..\Photos\changRes\dogInChairFrame0.jpg")
+fullFeaturesList = [None]*20
+fullLabelsList = [None]*20
+
+index = 0
+
+for i in range(10):
+    dog = ParsePhotoBB.parse_photo_BW(r"..\Photos\dog\dogInChairFrame" + str(i) + ".jpg")
+    notDog = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\dogNotInChairFrame" + str(i) + ".jpg")
+
+    fullFeaturesList[index] = dog
+    fullLabelsList[index]   = "Dog"
+
+    index += 1
+
+    fullFeaturesList[index] = notDog
+    fullLabelsList[index]   = "notDog"
+
+    index += 1
+
+list = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\dogNotInChairFrame37.jpg")
 clf = MLPClassifier()
-clf.fit([list], ["Dog"])
+clf.fit(fullFeaturesList, fullLabelsList)
 
 print clf.predict([list])
 
