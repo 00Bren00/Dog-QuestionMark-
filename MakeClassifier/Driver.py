e5a9af170ac6bbcb32f690405d2edb016d5a6765
00Bren00 <ringelb8@students.rowan.edu>
2018-04-06 21:37:56 -0400

    Almost works - we get a memory error - need to find a way to reduce the resolution on photos better.

--- MakeClassifier/Driver.py
+++ MakeClassifier/Driver.py
@@ -1,29 +1,58 @@
 import ParsePhotoBB
 from sklearn.neural_network import MLPClassifier
-from PIL import Image
+from sklearn.externals import joblib
 
-fullFeaturesList = [None]*20
-fullLabelsList = [None]*20
+fullFeaturesList = [None]*( 917 + 976)
+fullLabelsList = [None]*( 917 + 976)
 
 index = 0
 
-for i in range(10):
-    dog = ParsePhotoBB.parse_photo_BW(r"..\Photos\dog\dogInChairFrame" + str(i) + ".jpg")
-    notDog = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\dogNotInChairFrame" + str(i) + ".jpg")
+# for i in range(266):
+#     notDog = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\cat\cat" + str(i) + ".jpg")
+#     fullFeaturesList[index] = notDog
+#     fullLabelsList[index]   = "Not Dog"
+#     index += 1
+#
+#     #TODO get cats
 
+print "Parsing chair"
+
+for i in range(917):
+    notDog = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\NoDogJustChair\dogNotInChairFrame" + str(i) + ".jpg")
+    fullFeaturesList[index] = notDog
+    fullLabelsList[index]   = "Not Dog"
+    index += 1
+    #TODO get dogNotInChair
+
+print "parsing dog"
+
+for i in range(976):
+    dog = ParsePhotoBB.parse_photo_BW(r"..\Photos\dog\dogInChairFrame" + str(i) + ".jpg")
     fullFeaturesList[index] = dog
     fullLabelsList[index]   = "Dog"
-
     index += 1
 
-    fullFeaturesList[index] = notDog
-    fullLabelsList[index]   = "notDog"
+    #todo get all dog photos
 
-    index += 1
+# dog = ParsePhotoBB.parse_photo_BW(r"..\Photos\dog\dogInChairFrame" + str(i) + ".jpg")
+# notDog = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\dogNotInChairFrame" + str(i) + ".jpg")
+#
+# fullFeaturesList[index] = dog
+# fullLabelsList[index]   = "Dog"
+#
+# index += 1
+#
+# fullFeaturesList[index] = notDog
+# fullLabelsList[index]   = "notDog"
+#
+# index += 1
 
-list = ParsePhotoBB.parse_photo_BW(r"..\Photos\not-dog\dogNotInChairFrame37.jpg")
+list = ParsePhotoBB.parse_photo_BW(r"..\Photos\changRes\dogInChairFrame0.jpg")
+print "Fitting"
 clf = MLPClassifier()
 clf.fit(fullFeaturesList, fullLabelsList)
+
+joblib.dump(clf, r'..\Classifiers\Dog_Neural_Net.pkl')
 
 print clf.predict([list])
 
